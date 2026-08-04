392f780bc9deedf837c108bcb79dcca2b9d17632
Daniel Borges <daniel.m.borges@tecnico.ulisboa.pt>
2026-08-04 02:24:44 +0100

    fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lisbon.hackjunction.com/',
+	site: 'https://dsfjunctionx.github.io',
+	base: '/junctionx-website',
 	integrations: [mdx(), sitemap()],
 });
