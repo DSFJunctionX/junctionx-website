6b1bce9f6a78407f0cbf3033020b36a8a843d18b
duarte cruz <dudu.cruz2005@gmail.com>
2026-09-03 20:01:07 +0200

    Fix Astro build output directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://lisbon.hackjunction.com',
-	outDir: './docs',
+	outDir: './dist',
 	integrations: [mdx(), sitemap()],
 });
