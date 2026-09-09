ce7b622ef7e32a5c8fd731c9eb211613719e53ff
duarte cruz <dudu.cruz2005@gmail.com>
2026-09-09 18:13:58 +0200

    fix: stale cached website

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://lisbon.hackjunction.com',
-	outDir: './dist',
+	outDir: './docs',
 	integrations: [mdx(), sitemap()],
 });
